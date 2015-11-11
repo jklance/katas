f634fc892600d7b89ef76a5a0de4900b3343ee33
Jer Lance <jer.lance@perficient.com>
2015-11-11 18:43:23 -0500

    Adds test and solution for I

--- source/RomanNumeral.php
+++ source/RomanNumeral.php
@@ -1,3 +1,16 @@
 <?php
 
-class RomanNumeral {}
+class RomanNumeral {
+
+    private $_numeral = null;
+    private $_result  = null;
+
+    public function arabicToRoman($numeral) {
+        $this->_numeral = $numeral;
+
+        $this->_result  = 'I';
+
+        return $this->_result;
+    }
+
+}
--- tests/RomanNumeralTest.php
+++ tests/RomanNumeralTest.php
@@ -4,14 +4,14 @@ require_once dirname(__FILE__) . '/../source/RomanNumeral.php';
 
 class RomanNumeralTest extends PHPUnit_Framework_TestCase {
 
-    private $numeral;
+    private $_numeral;
 
     function setUp() {
         $this->_numeral = new RomanNumeral();
     }
 
     function testISubstitutions() {
-        $this->assertEquals('I', $this->numeral->arabicToRoman(1));
+        $this->assertEquals('I', $this->_numeral->arabicToRoman(1));
     }
     
 }
