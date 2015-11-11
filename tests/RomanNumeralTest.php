87a37155769d065ef9347cc8c112950c42b99384
Jer Lance <jer.lance@perficient.com>
2015-11-11 18:33:44 -0500

    Adds class; fixes test

--- tests/RomanNumeralTest.php
+++ tests/RomanNumeralTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__FILE__) . '/../RomanNumeral.php';
+require_once dirname(__FILE__) . '/../source/RomanNumeral.php';
 
 class RomanNumeralTest extends PHPUnit_Framework_TestCase {
 
