b67ee33be83b7423d2df737948bd153d4e1cd3e4
Jer Lance <me@jerlance.com>
2016-07-18 17:16:34 -0400

    Adds the ability to retrieve values for deposits

--- vending/lib/coins.js
+++ vending/lib/coins.js
@@ -6,6 +6,9 @@
     So that I can collect money from the customer
 */
 var Coin = function Constructor() {
+    this._depositedCoin = null;
+    this._depostValue   = null;
+    this._cashValue     = 0;
 
     return true;
 };
@@ -20,17 +23,22 @@ Coin.prototype.setCoin = function(coin) {
     return this._depositValue;
 }
 
+Coin.prototype.getCashValue = function() {
+    return this._cashValue;
+};
+
 Coin.prototype._getCoinValue = function() {
     if (this._depositedCoin) {
         if (this._depositedCoin == 'Q') {
-            this._cashValue += .25;
-            this._depositValue = .25;
+            this._depositValue = 25;
         } else if (this._depositedCoin == 'N') {
-            this._cashValue += .05;
-            this._depositValue = .05;
+            this._depositValue = 5;
         } else if (this._depositedCoin == 'D') {
-            this._depositValue = .1;
-            this._cashValue += this._depositValue;
+            this._depositValue = 10;
+        } else {
+            this._depositValue = 0;
         }
+        
+        this._cashValue += this._depositValue;
     }
 };
--- vending/spec/coins-spec.js
+++ vending/spec/coins-spec.js
@@ -14,15 +14,37 @@ beforeEach(function() {
 
 describe ("Can receive coins", function() {
     it ("will receive a quarter if deposited as 'Q'", function() {
-        expect(coin.setCoin('Q')).toBe(.25);
+        expect(coin.setCoin('Q')).toBe(25);
     });
     it ("will receive a quarter if deposited as 'q'", function() {
-        expect(coin.setCoin('q')).toBe(.25);
+        expect(coin.setCoin('q')).toBe(25);
     });
     it ("will receive a quarter if deposited as 'n'", function() {
-        expect(coin.setCoin('n')).toBe(.05);
+        expect(coin.setCoin('n')).toBe(5);
     });
     it ("will receive a quarter if deposited as 'd'", function() {
-        expect(coin.setCoin('d')).toBe(.1);
+        expect(coin.setCoin('d')).toBe(10);
     });
 });
+
+describe ("Can reject coins", function() {
+    it ("will reject a penny if deposited as 'p'", function() {
+        expect(coin.setCoin('p')).toBe(0);
+    });
+});
+
+describe ("Can return cash value", function() {
+    it ("will return zero if no deposits have been made", function() {
+        expect(coin.getCashValue()).toBe(0);
+    });
+    it ("will return a value if a series of deposits are made", function() {
+        coin.setCoin('q');
+        coin.setCoin('d');
+        coin.setCoin('n');
+        expect(coin.getCashValue()).toBe(40);
+    });
+});
+
+
+
+
